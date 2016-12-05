65332c0498e1b8dee6c73617f545ffe6afbe0a08
acabrerag <acabreraglz@hotmail.com>
2016-12-05 12:16:22 -0600

    Anadido trapecio

--- server.R
+++ server.R
@@ -12,27 +12,32 @@ library(shiny)
 # is increased from 5MB to 9MB
 # options(shiny.maxRequestSize = 9*1024^2)
 shinyServer(function(input,output,session){
-  
-  
   require(ggplot2)
-  #--------------------------------------Tarea 1------------------------------------------------------ 
+ 
+#-----------------------------------------
+  
   
   x <- reactive({
-    x    <-runif(input$bins,0,1)   # Old Faithful Geyser data
+    x    <-runif(input$nsims,0,1)   # Old Faithful Geyser data
     x=-1*(input$lambda)*log(x)
   })
   
   lambda <- reactive({
     lambda=input$lambda*1
   })
+  nbins <- reactive({
+    
+       nbins=input$nbins-1
+  })
+  
   output$distPlot <- renderPlot({
     
-    bins <- seq(min(x()), max(x()), length.out = input$bins + 1)
     
     # draw the histogram with the specified number of bins
-    hist(x(), breaks = 20, col = 'orange', border = 'white',main='Histograma de las observaciones',ylab='Frecuencia')
+    hist(x(), breaks = nbins(), col = 'orange', border = 'white',main='Histograma de las observaciones',ylab='Frecuencia')
     # estimate the parameters
     # Generate a summary of the data
+
     output$summary <- renderPrint({
       summary(x())
     })
@@ -59,7 +64,7 @@ shinyServer(function(input,output,session){
       ks.test(x(), "pexp", fit2$estimate) 
     })
     output$table <- renderTable({
-      
+
       data.frame(x())
     })
     output$pvalChi <- renderPrint({
@@ -83,97 +88,89 @@ shinyServer(function(input,output,session){
       })})
   
   
-  #--------------------------------------Tarea 2------------------------------------------------------ 
-  fun1 <- reactive({
-    texto <- paste("aux <- ", input$expresion1)
-    eval(parse(text=texto))
+ #-------------------------------------------------------------------------------------------- 
+
+  
+  #Tarea 2--------------------------------------------------------------------------
+  
+  I_trap <- reactive({
+    I<-sapply(1:input$N, function(k){
+      int_trap_mult(funMC(), rep(input$a[1],input$n), rep(input$a[2],input$n), k)
+    })
+    
+    out<-data.frame(I_trap=I)
+    
   })
-  
-  
-  fun2 <- reactive({
-    switch(input$expresion2,
-           "unif"= function(x) 1*(x>0 && x<1),
-           "exp"= function(x) dexp(x)*(x>0),
-           "norm" = function(x) dnorm(x)
-    )
-  })
-  
-  output$Grafica <- renderPlot({
-    x <- seq(input$xmin, input$xmax, length.out=100)
-    y1 <- sapply(x, fun1())
-    y2 <- input$M*sapply(x, fun2())
-    plot_limit = c(min(c(y1, y2)), max(c(y1, y2)))
-    # tarea: investigar sapply, lapply, apply, tapply, mapply
-    plot(x, y1, type="l", col="blue", ylab="y", ylim=plot_limit)
-    lines(x, y2, col="red")
-    legend("topright", c("f", "M*g"), col=c("blue", "red"), lty = 1)
-  })
-  
-  # 
-  
-  simulaciones <- reactive({
-    num_aceptados <- 0
-    num_intentos <- 0
-    sim_Y <- switch(input$expresion2,
-                    "unif"= function() runif(1),
-                    "exp"= function() rexp(1),
-                    "norm" = function() rnorm(1)
-    )
-    # print(sim_Y)
-    valor_aceptados <- numeric(input$nsim)
-    while(num_aceptados < input$nsim){
-      Y <- sim_Y()
-      U <- runif(1)
-      if(Y >= input$xmin && Y<=input$xmax && U <= (fun1()(Y))/(input$M*(fun2()(Y)))){
-        num_aceptados <- num_aceptados + 1
-        valor_aceptados[num_aceptados] <- Y
+  int_trap <- function(fx, a, b){
+    N <- length(fx)
+    (b - a)/2*mean(fx[-N] + fx[-1])
+  }
+  int_trap_mult <- function(f, a, b, N=20){
+    n <- length(a)
+    x <- seq(a[1], b[1], l=N)
+    if(n <= 1){
+      fx <- sapply(x, f)
+    } else{
+      fx <- numeric(N)
+      for(i in 1:length(x)){
+        g <- function(y){
+          f(c(x[i], y))
+        }
+        fx[i] <- int_trap_mult(g, a[-1], b[-1], N=N)
       }
-      num_intentos <- num_intentos + 1
     }
-    # print(valor_aceptados)
-    list(valor=valor_aceptados, tasa_exito=input$nsim/num_intentos)
+    I <- int_trap(fx, a[1], b[1])
+    return(I)
+  }
+  
+  
+  
+  
+  funMC <- reactive({
+    texto <- paste("aux <- ", input$expresionMC)
+    eval(parse(text=texto))
+    aux
   })
-  
-  output$tasa_exito <- renderText({
-    simulaciones()$tasa_exito
-  })
-  
-  output$hist_sim <- renderPlot({
-    hist(simulaciones()$valor, main="Histograma de las simulaciones", breaks=input$nbins)
-  })
-  
-  
-  #--------------------------------------Tarea 3------------------------------------------------------ 
+
   I_MC <- reactive({
-    I <- numeric(input$N)
+    
+    
+  I <- numeric(input$N)
     phix <- I
-    lower <- I
-    upper <- I
     s <- I
     for(i in 1:input$N){
       x <- runif(input$n, 0, 1)
       x<-x*(input$a[2]-input$a[1] )+input$a[1] 
-      texto <- paste("aux <- ", input$expresionMC)
-      
-      phix[i] <-eval(parse(text=texto))
-      
+      phix[i] <-eval(parse(text=substring(input$expresionMC,13)))
       s[i] <- sd(phix[1:i])
       I[i] <- (input$a[2]-input$a[1] )**input$n*mean(phix[1:i])
-      #Intervalos de confianza
-      lower[i] <- I[i] - s[i]/sqrt(i)*qnorm(1 - input$alpha/2, 0, 1)
-      upper[i] <- I[i] + s[i]/sqrt(i)*qnorm(1 - input$alpha/2, 0, 1)
+     
     }
+    out<-data.frame(I_MC=I,sd=s)
+  })
+  invento<-function(){
+    I<-I_MC()$I_MC
+    s<-I_MC()$sd
+    lower <- I
+    upper <- I
+    for(i in 1:input$N){
+    lower[i] <- I[i] - s[i]/sqrt(i)*qnorm(1 - input$alpha/2, 0, 1)
+    upper[i] <- I[i] + s[i]/sqrt(i)*qnorm(1 - input$alpha/2, 0, 1)   
+    }
+
     out <- data.frame(I_MC=I, lower, upper, sd=s)
-  })
+    
+  }
   data <- reactive({
-    cbind(nsim=1:input$N, I_MC())
+    if (input$trap){cbind(nsim=1:input$N,invento(),I_trap())
+}
+    else {cbind(nsim=1:input$N,invento())}
   })
   
-  
-  
+
+ 
   output$estim_MC <- renderUI({
     n <- nrow(data())
-    #paste0('\nEstimacion con ', n,' simulaciones : ',round(data()$I_MC[n], 3))
     texto=paste("\\int_{",input$a[1],"}^{",input$a[2],"}")
     alain<-input$n
     alain=alain-1
@@ -183,34 +180,26 @@ shinyServer(function(input,output,session){
     texto=paste(texto,"g(x)dx\\approx")
     if(alain==0){
       texto=paste("\\int_{",input$a[1],"}^{",input$a[2],"}g(x)dx\\approx")}
+    
+    if (input$trap){
+      return (withMathJax(paste("$$\\mbox{La estimacion de montecarlo con }",n,"\\mbox{ simulaciones es: }",texto,round(data()$I_MC[n], 3), "\\mbox{\n La estimacion del trapecio es }",texto,round(data()$I_trap[n], 3),"$$")))
+      
+    }
     return (withMathJax(paste("$$\\mbox{La estimacion con }",n,"\\mbox{ simulaciones es: }",texto,round(data()$I_MC[n], 3),"$$")))
   })
   
   output$plot <- renderPlot({
-    p <- ggplot(data(), aes(x=nsim)) +
+    p <- ggplot(data(), aes(x=log10(nsim))) +
       geom_line(aes(y=I_MC), color='blue', size=1)
-    if(input$ribbon){
-      p <- p + geom_ribbon(aes(ymin=lower, ymax=upper), fill='orange', alpha=0.5)
-    }
-    p +
-      labs(x='Numero de simulaciones',
-           y='I',
-           title='Estimacion de la integral')
+    if(input$trap){p<-p+geom_line(aes(y=I_trap), color='red', size=1)}
+    if(input$ribbon){p <- p + geom_ribbon(aes(ymin=lower, ymax=upper), fill='orange', alpha=0.5)}
+      p + labs(x='Número de simulaciones en logaritmo base 10', y='Estimación de la integral')
   })
   output$data <- renderDataTable(round(data(), 3))
   
-  observeEvent(input$reset_input, {
-    updateNumericInput(session, "n", value = 1)
-    updateSliderInput(session, "N", value = 50)
-    updateSliderInput(session, "a", value = c(0,2))
-    updateSliderInput(session, "alpha", value = 0.05)
-    updateCheckboxInput(session, "ribbon", value = TRUE)
-  })
+
   
-  
-  
-  
-  
+
 })
 
 
--- ui.R
+++ ui.R
@@ -2,109 +2,114 @@ library(shiny)
 
 
 shinyUI(fluidPage(
-  titlePanel("Alain Cabrera"),
-  sidebarLayout(
-    #--------------------------------------------NOMBRE TAREAS-----------------------------------------------      
-    sidebarPanel(
-      selectInput("opcion", "Tarea:", choices = c("Simulador Exponencial", "Region de rechazo",
-                                                  "Integrales-Montecarlo"), selected = 'Integrales-Montecarlo'),
-      
-      #--------------------------<<<<<<<<<<<<<------------INPUTS----------------->>>>>>>>>>>>>>>>>>>>>>---------------------------------------      
-      
-      #-----------------------------------TAREA 3---------------------------------------------------------          
-      
-      conditionalPanel(
-        'input.opcion === "Integrales-Montecarlo"',
-        numericInput('n', label = 'Dimension de la funcion', value = 1, min = 1, max = 5),
-        textInput(
-          inputId="expresionMC", 
-          label="Funcion g(x)",
-          value="mean(x)"
+    titlePanel("Alain Cabrera"),
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("opcion", "Tarea:", choices = c("Tarea 1", "Tarea 2","Tarea 4"), selected = 'Tarea 2'),
+            
+ #----------------------------------.
+ conditionalPanel(
+              'input.opcion === "Tarea 4"',
+              
+              selectInput("dep", "Variable Dependiente:", choices = colnames(swiss), selected = 'Fertility'), 
+              checkboxGroupInput("checkGroup", label = "Variables Independientes", 
+                                 choices = colnames(swiss),
+                                 selected = 1),
+            sliderInput('N4', label = 'Numero de simulaciones', value = 50, min = 1, max = 2000, step = 1),
+              sliderInput('alpha4', label = 'Longitud de la cadena', min = 100, max = 2000, value =1000, step = 10),actionButton('Calcular', 'Calcular')),
+            
+            
+#-------------------------------------------------------------------------------            
+            conditionalPanel(
+              'input.opcion === "Tarea 2"',
+            
+            numericInput('n', label = 'Dimension de la funcion', value = 2, min = 1, max = 5),
+            textInput(
+              inputId="expresionMC", 
+              label="Funcion g(x)",
+              value="function(x) 12*x[1]^2/(1+x[2]^2)"
+            ),
+            sliderInput('a', label = 'Limites de integracion', min = -6, max = 6, value = c(0,1), step = 0.5),
+            #sliderInput('N', label = 'Numero de simulaciones', value = 100, min = 1, max = 1000, step = 100),
+            selectInput("N", "Numero de simulaciones:", choices = c(10,100,1000,10000), selected = 100), 
+            
+          sliderInput('alpha', label = 'Significancia del intervalo', min = 0.001, max = 0.1, value = 0.05, step = 0.001)),
+            
+  #------------------------------------------------------------------------------------------------------------          
+            conditionalPanel(
+                'input.opcion === "Tarea 1"',
+                sliderInput('nsims', label = 'Numero de simulaciones', value = 1000, min = 10, max = 2000, step = 1),
+                sliderInput('nbins', label = 'Numero de clases', value = 5, min = 2, max = 10, step = 1),
+
+                  numericInput("lambda",
+                               "Parametro:",
+                               value = 1,step=.1),
+                  downloadButton('downloadData', 'Download')
+                  
+              
+            )
+            
+            
         ),
-        sliderInput('a', label = 'Limites de integracion', min = -6, max = 6, value = c(0,2), step = 0.01),
+  #------------------------------------------------------------------------------
+        mainPanel(
+          conditionalPanel(condition="input.opcion=='Tarea 1'",
+            tabsetPanel(type = 'tabs',
+                        tabPanel("Histograma", plotOutput("distPlot") , h1('Prueba de bondad de ajuste'), 
+                                 p(textOutput("pvalChi"),textOutput("pvalK"))),
+                        tabPanel("Observaciones", h4('A continuacion se presentan las primeras 10 simulaciones obtenidas.')
+                                                     ,h4('Para descargar un archivo csv con todas las simulaciones, favor de oprimir el boton Download.'),tableOutput("table")),
+                        tabPanel("Descriptivos",h4('A continuacion se presentan los estadisticos descriptivos de la muestra obtenida.') ,verbatimTextOutput("summary"),
+                                 h4('Los valores teoricos son: '),  h5(textOutput('media')),h5(textOutput('q1')),h5(textOutput('mediana')) ,h5(textOutput('q3')))
+        ))
+        ,
         
-        sliderInput('N', label = 'Numero de simulaciones', value = 50, min = 1, max = 2000, step = 1),
-        sliderInput('alpha', label = 'Significancia del intervalo', min = 0.001, max = 0.1, value = 0.05, step = 0.001),
-        actionButton('reset_input', 'Reset input')),
-      
-      
-      #------------------------------------TAREA 2------------------------------------------------------------------------          
-      
-      conditionalPanel('input.opcion === "Region de rechazo"', withMathJax(),
-                       
-                       h2("Aceptacion-Rechazo"),
-                       textInput(
-                         inputId="expresion1", 
-                         label="$$\\mbox{Funcion } f$$",
-                         value="function(x) 2*x"
-                       ),
-                       selectInput(
-                         inputId="expresion2", 
-                         label="$$\\mbox{Funcion } g$$",
-                         choices=c("Uniforme(xmin, xmax)"="unif", "Exponencial(1) truncada a (xmin,xmax)"="exp", "Normal(0,1) truncada a (xmin,xmax)"="norm")
-                       ),
-                       sliderInput("xmin", "xmin", min=-30, max=30, value=0),
-                       sliderInput("xmax", "xmax", min=-30, max=20, value=1),
-                       sliderInput("M", "$$M$$", min=0.1, max=100, value=1),
-                       numericInput("nsim", "Numero de simulaciones", value=100)
-                       
-      )
-      
-      
-    ),
-    
-    #--------------------------<<<<<<<<<<<<<------------OUTPUTS----------------->>>>>>>>>>>>>>>>>>>>>>---------------------------------------      
-    
-    mainPanel(
-      
-      
-      #------------------------------------TAREA 1------------------------------------------------------------------------          
-      conditionalPanel(condition="input.opcion=='Simulador Exponencial'",
-                       tabsetPanel(type = 'tabs',
-                                   tabPanel("Histograma", plotOutput("distPlot") , h1('Prueba de bondad de ajuste'), 
-                                            p(textOutput("pvalChi"),textOutput("pvalK"))),
-                                   tabPanel("Observaciones", h4('A continuacion se presentan las primeras 10 simulaciones obtenidas.')
-                                            ,h4('Para descargar un archivo csv con todas las simulaciones, favor de oprimir el boton Download.'),tableOutput("table")),
-                                   tabPanel("Descriptivos",h4('A continuacion se presentan los estadisticos descriptivos de la muestra obtenida.') ,verbatimTextOutput("summary"),
-                                            h4('Los valores teoricos son: '),  h5(textOutput('media')),h5(textOutput('q1')),h5(textOutput('mediana')) ,h5(textOutput('q3')))
-                       )),
-      
-      
-      
-      #------------------------------------TAREA 2------------------------------------------------------------------------          
-      conditionalPanel(condition="input.opcion=='Region de rechazo'",
-                       tabsetPanel(
-                         tabPanel("Grafica de la region",h2('Region de rechazo'), h4('Objetivo: Simular observaciones de la distribución f'), 
-                                  h4('Descripción del algoritmo:'), 
-                                  withMathJax()
-                                  , h5("$$\\cdot\\mbox{Generar independientemente }U\\sim Unif[0,1]\\mbox{ y  }Y\\mbox{ de acuerdo a la densidad g. }$$")
-                                  , h5("$$\\cdot\\mbox{Si }U\\leq\\frac{f(y)}{Mg(Y)}\\mbox{ se acepta el conjunto  }X=Y\\mbox{. En otro caso, volver al caso anterior y volver a empezar. }$$")
-                                  , h5("$$\\mbox{ El conjunto resultante }X\\mbox{  se distribuye de acuerdo a la función de densidad }f$$")
-                                  , plotOutput("Grafica")),
-                         tabPanel("Resultados", p("Tasa de exito", textOutput("tasa_exito")),h1('Prueba de bondad de ajuste'),plotOutput("hist_sim") ,
-                                  sliderInput("nbins", "nbins", value=20, min=10, max=100)
-                         )
-                       ))
-      
-      #------------------------------------TAREA 3------------------------------------------------------------------------          
-      , conditionalPanel(condition="input.opcion=='Integrales-Montecarlo'",
-                         tabsetPanel(
-                           #         tabPanel('Hist', plotOutput('random_hist')),
-                           tabPanel('Grafica',h4('Esta app permite estimar el valor de una integral sobre un hiper-cubo.'),
-                                    withMathJax(uiOutput('estim_MC')),
-                                    plotOutput('plot', width = '8in', height = '3in'),
-                                    checkboxInput('ribbon', 'Mostrar intervalos de confianza', value = TRUE)
+        conditionalPanel(condition="input.opcion=='Tarea 4'",
+                         tabsetPanel(type = 'tabs',
+                           tabPanel('Gráfico de dispersión',h4('Esta app permite estimar el valor de una integral sobre un hiper-cubo.')
                                     
                            ),
-                           tabPanel('Datos', dataTableOutput('data'))
-                         )
-                         
-      )
-      
-      
-      
-      
-    )
-  )))
+                           
+                           tabPanel('Histogramas',h4('Esta app permite estimar el valor de una integral sobre un hiper-cubo.')
+                                    
+                           ),
+                           
+                           
+                           tabPanel('Densidad',h4('Esta app permite estimar el valor de una integral sobre un hiper-cubo.')
+                                    
+                           ),
+                           tabPanel('Datos', h4('Esta app permite estimar el valor de una integral sobre un hiper-cubo.'))))
+        
+        ,
+        
+ conditionalPanel(condition="input.opcion=='Tarea 2'",
+                   tabsetPanel(
+                     #         tabPanel('Hist', plotOutput('random_hist')),
+                     tabPanel('Estimación',
+                              h5('DESCRIPCIÓN: Esta app permite estimar el valor de una integral múltiple sobre un hiper-cubo via método de Montecarlo y Regla del Trapecio.'),
+                              h5('NOTA: La función g(x) toma como variables a las entradas del vector x, es decir, x[1] corresponde a la variable x, x[2] a la variable y.')
+                              ,h5('EJEMPLO: En dimensión 2, function(x) 12*x[1]^2/(1+x[2]^2) representaría: '),
+                              withMathJax("$$\\int_{0}^{1}\\int_{0}^{1}\\frac{12x^2}{1+y^2}dxdy=\\pi$$"),
+                              h4('RESULTADO:'),
+                              withMathJax(uiOutput('estim_MC')),
+                              plotOutput('plot', width = '8in', height = '3in'),
+                              checkboxInput('ribbon', 'Mostrar intervalos de confianza', value = TRUE),
+                              checkboxInput('trap', 'Comparar con método del trapecio', value = TRUE),
+                              h4('ADVERTENCIA: No se recomienda hacer la comparación con el método del trapecio para dimensiones altas ni para número de simulaciones (cortes uniformes) altos.')
+                              
+
+                     ),
+                     tabPanel('Datos', dataTableOutput('data'))
+                   )
+                   
+                   
+                   
+                   )
+
+
+        
+        
+)
+)))
 
 
